a709f95b608dad8eec9b192f55982570d2145433
00402720 <77757784+00402720@users.noreply.github.com>
2025-08-26 16:59:34 -0600

    Add files via upload

---
+++ swish.pl
@@ -0,0 +1,45 @@
+tiene(aldemir, aldea).
+
+pilar(familia).
+pilar(oficios).
+pilar(alianzas).
+
+vive(don_esteban, rivoria).
+vive(dona_mariela, rivoria).
+vive(clara,rivoria).
+vive(rodolfo ,rivoria).
+vive(ismael ,rivoria).
+vive(andres ,rivoria).
+vive(sofia ,rivoria).
+vive(teresa ,rivoria).
+vive(lucas ,rivoria).
+vive(martina ,rivoria).
+vive(don_alonso,montelar).
+vive(dona_beatriz,montelar).
+
+
+casada(dona_mariela,riviora).
+
+hijo(sofia,clara).
+hijo(sofia,andres).
+hijo(lucas,rodolfo).
+hijo(lucas,teresa).
+hijo(martina,rodolfo).
+hijo(martina,teresa).
+hijo(clara,don_esteban).
+hijo(calra,dona_mariela).
+hijo(rodolfo,don_esteban).
+hijo(rodolfo,dona_mariela).
+hijo(ismael,don_esteban).
+hijo(ismael, dona_mariela).
+hijo(felipe,don_alonso).
+hijo(felipe,dona_beatriz).
+hijo(catalina,don_alonso).
+hijo(catalina_dona_beatriz).
+hijo(emilia,don_alonso).
+hijo(emilia,dona_catalina).
+explorador(felipe).
+
+amigo(ismael,hector).
+
+herrero(hector).
